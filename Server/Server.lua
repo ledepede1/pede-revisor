319bf167d9ca27031f4a7b31fc80b690ccdb54fb
Mikkel <142742362+ledepede1@users.noreply.github.com>
2023-12-29 17:52:26 +0100

    Update Server.lua

--- Server/Server.lua
+++ Server/Server.lua
@@ -1,5 +1,23 @@
 ESX = exports["es_extended"]:getSharedObject()
 
+function CheckIfPlayerIsAllowed(player)
+    if player == nil then
+    player = source
+    end
+
+    local xPlayer = ESX.GetPlayerFromId(player)
+    local hasJob = false
+
+    for _, v in pairs(Config.Companys) do
+        if v.JobName == xPlayer.job.name then
+                hasJob = true
+            break
+        end
+    end
+
+    return hasJob
+end
+
 for _, v in pairs(Config.Companys) do
 
     TriggerEvent('esx_society:registerSociety', v.JobName, v.CompaneName, 'society_'..v.JobName, 'society_'..v.JobName, 'society_'..v.JobName, {type = 'private'})
@@ -19,48 +37,45 @@ for _, v in pairs(Config.Companys) do
 end
 
 RegisterNetEvent("checkBlackMoney:Pede")
-AddEventHandler("checkBlackMoney:Pede", function (money, percent, society, jobname)
+AddEventHandler("checkBlackMoney:Pede", function (money, percent, society)
     local source = source
     local xPlayer = ESX.GetPlayerFromId(source)
     local blackMoney = xPlayer.getAccount('black_money').money
-    local job = xPlayer.job.name
 
-
-    if job ~= jobname then
+    if not CheckIfPlayerIsAllowed(source) then
         Log("Playername: "..GetPlayerName(source).."\n Got kicked for exploiting keep an eye out!")
         Citizen.Wait(50)
         xPlayer.kick("EXECUTING")
-    end
-
-    local company = 'society_'..society
-
-    local companyFetched = MySQL.Sync.fetchAll('SELECT name FROM `addon_account` WHERE `name` = ?', {
-        company
-    })
-
-
-    if blackMoney >= money then
-        if companyFetched ~= nil then
-        local amount = money / 100 * percent
-
-        local waitTime = money * Config.Time
-        TriggerClientEvent("ProgressBar:Pede", source, money, waitTime)
-
-        Citizen.Wait(waitTime)
-
-        TriggerEvent('esx_addonaccount:getSharedAccount', company, function(account)
-            account.addMoney(money-amount)
-
-            xPlayer.removeAccountMoney('black_money', money)
-            xPlayer.addMoney(amount)
-
-            Log("Navn: "..GetPlayerName(source).."\n Har lige hvidvasket "..money..Config.Valuta.."\n Og valgte "..percent.."% og modtog "..amount.." ud af det!")
-		end)
     else
-        print("ERROR: Companys society dosent exist")
-    end
+        local company = 'society_'..society
+        local companyFetched = MySQL.Sync.fetchAll('SELECT name FROM `addon_account` WHERE `name` = ?', {
+            company
+        })
+
+
+        if blackMoney >= money then
+            if companyFetched ~= nil then
+            local amount = money / 100 * percent
+
+            local waitTime = money * Config.Time
+            TriggerClientEvent("ProgressBar:Pede", source, money, waitTime)
+
+            Citizen.Wait(waitTime)
+
+            TriggerEvent('esx_addonaccount:getSharedAccount', company, function(account)
+                account.addMoney(money-amount)
+
+                xPlayer.removeAccountMoney('black_money', money)
+                xPlayer.addMoney(amount)
+
+                Log("Navn: "..GetPlayerName(source).."\n Har lige hvidvasket "..money..Config.Valuta.."\n Og valgte "..percent.."% og modtog "..amount.." ud af det!")
+            end)
         else
-            TriggerClientEvent("notify:pede", source, Config.Notifications.NotEnoughMoney.Title, Config.Notifications.NotEnoughMoney.Description, Config.Notifications.NotEnoughMoney.Type)
+            print("ERROR: Companys society dosent exist")
+        end
+            else
+                TriggerClientEvent("notify:pede", source, Config.Notifications.NotEnoughMoney.Title, Config.Notifications.NotEnoughMoney.Description, Config.Notifications.NotEnoughMoney.Type)
+        end
     end
 end)
 
